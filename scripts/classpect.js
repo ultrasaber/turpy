32d9592c278a49616db0043ed1930314f0310fe4
A real Saber <ultrasaber@users.noreply.github.com>
2017-07-20 23:57:55 -0500

    added classpect.js

---
+++ scripts/classpect.js
@@ -0,0 +1,17 @@
+client.on('message', message => {
+    var command = turpy.getCommand(message.content);
+    
+    if (command === 'classpect')
+    {
+        var ID = message.author.id;
+        
+        var classArray = ['Maid', 'Page', 'Mage', 'Knight', 'Rogue', 'Sylph', 'Seer', 'Thief', 'Heir', 'Bard', 'Prince', 'Witch', 'Lord', 'Muse'];
+        var aspectArray = ['Time', 'Breath', 'Doom', 'Blood', 'Heart', 'Space', 'Mind', 'Light', 'Void', 'Rage', 'Hope', 'Life'];
+        
+        var classUser = ID % classArray.length;
+        var aspectUser = ID % aspectArray.length;
+        
+        message.reply('Your classpect has been determined to be the ' + classArray[classUser] + ' of ' + aspectArray[aspectUser] + '.');
+    }
+
+});
