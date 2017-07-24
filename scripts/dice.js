f4b98eb4f1aa12ab40abff73e8899c61efc98b0d
A real Saber <ultrasaber@users.noreply.github.com>
2017-07-24 11:45:33 -0500

    added dice.js

---
+++ scripts/dice.js
@@ -0,0 +1,35 @@
+const MAXIMUM_ROLLS = 100;
+const MAXIMUM_SIDES = 300;
+
+client.on('message', message => {
+    var command = turpy.getCommand(message.content);
+
+    if (command) {
+        if (command.match(/^roll ([0-9]+)d([0-9]+)/) !== null) {
+            var parsedCommand = command.match(/^roll ([0-9]+)d([0-9]+)/);
+
+            var dice = parsedCommand[1];
+            var sides = parsedCommand[2];
+
+            if ((dice > MAXIMUM_ROLLS) || (sides > MAXIMUM_SIDES)) {
+                message.reply("*no.*");
+            }
+            else {
+                var total = 0;
+                var output = " (";
+
+                for(var i = 0; i < dice; i++) {
+                    var roll = Math.floor((Math.random() * sides) + 1);
+
+                    output += roll + ((i < (dice-1)) ? " + " : "");
+
+                    total += roll;
+                }
+
+                output += ")";
+
+                message.reply(":game_die: You rolled **" + total + "**:" + output);
+            }
+        }
+    }
+});
