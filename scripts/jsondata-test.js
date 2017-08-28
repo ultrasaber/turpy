05bd352d2a41262fb515d92e6ece347d98faea5e
A real Saber <ultrasaber@users.noreply.github.com>
2017-08-28 12:56:03 -0500

    include test script for json - #2

---
+++ scripts/jsondata-test.js
@@ -0,0 +1,12 @@
+var testObject = {
+    foo: "bar",
+    bar: "foo"
+}
+
+turpy.writeJSONData('data/global', 'json-test', testObject, () => {
+    turpy.log('[TEST] Successully wrote data.');
+
+    turpy.readJSONData('data/global', 'json-test', (readObject) => {
+        turpy.log(util.inspect(readObject));
+    });
+});
