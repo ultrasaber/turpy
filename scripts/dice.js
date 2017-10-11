7497401e3ca32a2c9e2d5687b36612e8f1986a3c
A real Saber <ultrasaber@users.noreply.github.com>
2017-10-11 14:18:27 -0500

    Implement #4

--- scripts/dice.js
+++ scripts/dice.js
@@ -3,35 +3,40 @@ const MAXIMUM_SIDES = 300;
 
 turpy.addHelpText('roll XdY', 'Roll some dice.');
 
+function rollDice(dice, sides) {
+    if ((dice > MAXIMUM_ROLLS) || (sides > MAXIMUM_SIDES)) {
+        return "*no.*";
+    }
+    else {
+        var total = 0;
+        var output = " (";
+
+        for(var i = 0; i < dice; i++) {
+            var roll = Math.floor((Math.random() * sides) + 1);
+
+            output += roll + ((i < (dice-1)) ? " + " : "");
+
+            total += roll;
+        }
+
+        output += ")";
+
+        return ":game_die: You rolled **" + total + "**:" + output;
+    }
+}
+
 turpy.client.on('message', message => {
     var command = turpy.getCommand(message.content);
 
     if (command) {
-        if (command.match(/^roll ([0-9]+)d([0-9]+)/) !== null) {
+        if (command.match(/^roll ([0-9]+)d([0-9]+)$/) !== null) {
             var parsedCommand = command.match(/^roll ([0-9]+)d([0-9]+)/);
+            message.reply(rollDice(parsedCommand[1], parsedCommand[2]));
+        }
 
-            var dice = parsedCommand[1];
-            var sides = parsedCommand[2];
-
-            if ((dice > MAXIMUM_ROLLS) || (sides > MAXIMUM_SIDES)) {
-                message.reply("*no.*");
-            }
-            else {
-                var total = 0;
-                var output = " (";
-
-                for(var i = 0; i < dice; i++) {
-                    var roll = Math.floor((Math.random() * sides) + 1);
-
-                    output += roll + ((i < (dice-1)) ? " + " : "");
-
-                    total += roll;
-                }
-
-                output += ")";
-
-                message.reply(":game_die: You rolled **" + total + "**:" + output);
-            }
+        if (command.match(/^roll ([0-9]+)$/) !== null) {
+            var parsedCommand = command.match(/^roll ([0-9]+)/);
+            message.reply(rollDice(1, parsedCommand[1]));
         }
     }
 });
