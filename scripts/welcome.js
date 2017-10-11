1e17f848b1ee13b2dbefb86eae8f45d4cddd3e94
A real Saber <ultrasaber@users.noreply.github.com>
2017-10-11 18:39:41 -0500

    #5 disable welcome script

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,5 +1,7 @@
 // Displays a welcome message for new users.
 
+/*
 turpy.client.on('guildMemberAdd', member => {
   member.guild.defaultChannel.send(`${member}, welcome to ${member.guild.name}! Enjoy your stay.`);
 });
+*/
